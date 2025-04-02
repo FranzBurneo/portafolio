422eeda223cb3939c80f8194a8412766f891fea6
FranzBurneo <franzdavidburneo@gmail.com>
2025-04-02 01:15:24 -0500

    feat: download cv and view projects

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import i18next from 'i18next';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports: [RouterModule]
 })
 export class HomeComponent {
   t = i18next.t;
+  cvUrl = 'assets/FranzBurneo_es65.pdf';
+
+  constructor() {
+    this.setCvUrl(i18next.language);
+
+    // Escuchar cambios de idioma dinámicos
+    i18next.on('languageChanged', (lng) => {
+      this.setCvUrl(lng);
+    });
+  }
+
+  private setCvUrl(lang: string) {
+    this.cvUrl = lang === 'en'
+      ? 'assets/FranzBurneo_en5.pdf'
+      : 'assets/FranzBurneo_es65.pdf';
+  }
 }
