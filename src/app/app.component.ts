f57134598d9dd70c48594a637be92cdd67b186f0
FranzBurneo <franzdavidburneo@gmail.com>
2025-04-02 01:03:55 -0500

    fix: leaguaje select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import i18next from './core/i18n'; // ajusta la ruta si está en otro lado
+import i18next from './core/i18n';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  selectedLang = 'es';
 
-  // Getter válido
   get t() {
     return i18next.t;
   }
 
-  // Método para cambiar idioma
   changeLang(lang: string) {
+    this.selectedLang = lang;
     i18next.changeLanguage(lang);
+
     if (typeof window !== 'undefined') {
       localStorage.setItem('lang', lang);
     }
-  }  
+  }
 
   constructor() {
     if (typeof window !== 'undefined') {
       const savedLang = localStorage.getItem('lang') || 'es';
+      this.selectedLang = savedLang;
       i18next.changeLanguage(savedLang);
     } else {
-      i18next.changeLanguage('es'); // fallback por si estás en SSR
+      // fallback para entornos sin acceso al window
+      this.selectedLang = 'es';
+      i18next.changeLanguage('es');
     }
-  }  
+  }
 }
